330bada48c9493a8d2bca4b17120e235c094e7e4
DONGUK777 <kimdongyork@gmail.com>
2024-09-25 09:30:13 +0900

    add prediction fun

--- src/mnist/worker.py
+++ src/mnist/worker.py
@@ -3,6 +3,11 @@ from mnist.db import select, dml
 import random
 import os
 import requests
+import numpy as np
+from PIL import Image
+from keras.models import load_model
+from tensorflow.keras.datasets import mnist
+
 
 def get_job_img_task():
     sql = """
@@ -20,6 +25,33 @@ def get_job_img_task():
     else:
         return None
 
+def get_model():
+    # 모델 로드
+    model_path = os.getenv('MODEL_PATH', '/home/tommy/code/mnist/note/mnist240924.keras')
+    model = load_model(f'{model_path}')
+
+    return model
+
+
+# 사용자 이미지 불러오기 및 전처리
+def preprocess_image(image_path):
+    img = Image.open(image_path).convert('L')  # 흑백 이미지로 변환
+    img = img.resize((28, 28))  # 크기 조정
+
+    # 흑백 반전
+    img = 255 - np.array(img)  # 흑백 반전
+
+    img = np.array(img)
+    img = img.reshape(1, 28, 28, 1)  # 모델 입력 형태에 맞게 변형
+    img = img / 255.0  # 정규화
+    return img
+    
+def predict_digit(image_path):
+    img = preprocess_image(image_path)
+    prediction = model.predict(img)
+    digit = np.argmax(prediction)
+    return digit
+
 def prediction(file_path, num):
     sql = """UPDATE image_processing
     SET prediction_result=%s,
@@ -27,45 +59,43 @@ def prediction(file_path, num):
         prediction_time=%s
     WHERE num=%s
     """
-    presult = random.randint(0, 9)
-    dml(sql, str(presult), jigeum.seoul.now(), num)
-    
+    presult = predict_digit(file_path) 
+    model = get_model()
+    dml(sql, presult, now(), num)
     return presult
 
+# image_path = '/home/tommy/code/mnist/note/train_img/2_1.png'
+
 def run():
-  """image_processing 테이블을 읽어서 가장 오래된 요청 하나씩을 처리"""
+
+    """image_processing 테이블을 읽어서 가장 오래된 요청 하나씩을 처리"""
   
-  # STEP 1
-  # image_processing 테이블의 prediction_result IS NULL 인 ROW 1 개 조회 - num 가져오기
-  job = get_job_img_task()
+    # STEP 1
+    # image_processing 테이블의 prediction_result IS NULL 인 ROW 1 개 조회 - num 가져오기
+    job = get_job_img_task()
 
-  if job is None:
-      print(f"{jigeum.seoul.now()} - job is None")
-      return 
+    if job is None:
+        print(f"{jigeum.seoul.now()} - job is None")
+        return 
 
-  num = job['num']
-  file_name = job['file_name']
-  file_path = job['file_path']
+    num = job['num']
+    file_name = job['file_name']
+    file_path = job['file_path']
+    
+    # STEP 2
+    presult = prediction(file_path, num)
+    print(jigeum.seoul.now())
+    
+    # STEP 3
+    # LINE 으로 처리 결과 전송
+    send_line_noti(file_name, presult)
 
-
-  # STEP 2
-  # RANDOM 으로 0 ~ 9 중 하나 값을 prediction_result 컬럼에 업데이트
-  # 동시에 prediction_model, prediction_time 도 업데이트
-  presult = prediction(file_path, num)
-
-  # STEP 3
-  # LINE 으로 처리 결과 전송
-  send_line_noti(file_name, presutl)
-
-  print(jigeum.seoul.now())
-
-def send_line_noti():
-    KEY = os.environ.get('LINE_NOTI_TOKEN')
+def send_line_noti(file_name, presult):
+    KEY = os.environ.get('LINE_NOIT_TOKEN')
     url = "https://notify-api.line.me/api/notify"
-    data = {"message": "성공적으로 저장했습니다!"}
-    headers = {"Authorization": 'Bearer '+KEY}
+    data = {"message": f"{file_name} -> {presult}"}
+    headers = {"Authorization": "Bearer " + KEY}
     response = requests.post(url, data=data, headers=headers)
     print(response.text)
     print("SEND LINE NOTI")
-
-send_line_noti()
+run()
